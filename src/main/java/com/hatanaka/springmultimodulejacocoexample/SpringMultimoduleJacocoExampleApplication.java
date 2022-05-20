f1dc4b93cfa10a8bcc37760211d433c0fe57490f
Daniel Hatanaka <dhatanaka@uolinc.com>
2022-05-20 10:22:41 -0300

    first commit

---
+++ src/main/java/com/hatanaka/springmultimodulejacocoexample/SpringMultimoduleJacocoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hatanaka.springmultimodulejacocoexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMultimoduleJacocoExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMultimoduleJacocoExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hatanaka/springmultimodulejacocoexample/SpringMultimoduleJacocoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatanaka.springmultimodulejacocoexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMultimoduleJacocoExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
